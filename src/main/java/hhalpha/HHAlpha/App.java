cb81cedd25563c849792885f0d5fdaccd024de0f
arpit108 <arpit.iitdhn@gmail.com>
2015-10-11 16:51:26 +0530

    Alpha Changes

---
+++ src/main/java/hhalpha/HHAlpha/App.java
@@ -0,0 +1,13 @@
+package hhalpha.HHAlpha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
